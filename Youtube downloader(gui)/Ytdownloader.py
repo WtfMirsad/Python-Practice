ba244cbe16e751ec493c9e99cfb23c76938c24b7
Mirsad Zukic <salihzukic76@gmail.com>
2021-01-07 15:57:25 +0100

    Updates to file names and files present in the folder

--- Youtube downloader(gui)/Ytdownloader.py
+++ Youtube downloader(gui)/Ytdownloader.py
@@ -4,20 +4,21 @@ import os
 
 sg.theme('DarkAmber')
 
-layout = [  [sg.Text('U sljedecem redu upisi ')],
-            [sg.Text('Enter something on Row 2'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
+layout = [  [sg.Text('U sljedecem redu upisi URL ')],
+            [sg.Text('Video URL: '), sg.InputText()],
+            [sg.Button('Download'), sg.Button('Cancel')] ]
 
 # Create the Window
 window = sg.Window('YT Video downloader', layout)
+
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
     url = values[0]
     link = url.strip()
-    os.system("start cmd /k python main.py " + link)
+    os.system("start cmd /k python downloader.py " + link)
+
     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
         break
-    print('You entered ', values[0])
 
 window.close()
